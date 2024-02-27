15cd31325f61028973858a60e873a0e3a4df24ab
blackswitch <jhkang1517@gmail.com>
2024-02-27 14:47:49 +0900

    initial commit

---
+++ src/test/java/com/blackswitch/batchpractice/BatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackswitch.batchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
